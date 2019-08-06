b30dfdbd8249826e897ccec54afa7f93cfa7a1bf
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-08-05 21:30:49 -0700

    Add new ip

--- server/app.js
+++ server/app.js
@@ -1,39 +1,74 @@
+require('newrelic');
 const path = require('path');
 const express = require('express');
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT;
+const client = redis.createClient(REDIS_PORT);
 
+const request =require('request');
 const app = express();
 app.use(express.static(path.join(__dirname, '/../public/')));
 app.use('/:restaurant_id', express.static(path.join(__dirname, '/../public/')));
 
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
-const gallery = 'http://13.57.252.210',
+
+const gallery = 'http://54.183.221.234:3001',
     reservations = 'http://54.200.32.135',
     menus = 'http://18.219.221.244',
     reviews = 'http://18.223.115.5';
 
-app.get("/:restaurant_id/images", function(req, res) {
-    console.log('redirecting to photo gallery server');
-    apiProxy.web(req, res, {target: gallery});
+    const kat = 'http://18.223.132.108:3005/booking/1/'
+
+    function cache(req, res, next) {
+      const param = req.params.restaurant_id;
+      client.get(param, function (err, data) {
+          if (err) throw err;
+          if (data != null) {
+          let daga =JSON.parse(data)
+              res.send(daga);
+          } else {
+              next();
+          }
+      });
+    }
+
+app.get("/:restaurant_id/images", cache, function(req, res) {
+  request(`http://54.183.221.234:3001/${req.params.restaurant_id}/images`, (error, response, body) => {
+    if (error){
+      console.log(error)
+    }else{
+      client.set(req.params.restaurant_id, JSON.stringify(body));
+      res.status(200).send(result.rows);
+      
+    }
+
+  
+});
+    // console.log('redirecting to photo gallery server');
+    // apiProxy.web(req, res, {target: gall}, (err,result) => {
+    //   console.log('dd')
+    // });
+    
 });
 
 app.all("/:restaurant_id/reservations", function(req, res) {
-  console.log('redirecting to reservations');
+  //console.log('redirecting to reservations');
   apiProxy.web(req, res, {target: reservations});
 });
 
 app.all("/:restaurant_id/reservations/*", function(req, res) {
-    console.log('redirecting to reservations');
+   // console.log('redirecting to reservations');
     apiProxy.web(req, res, {target: reservations});
 });
 
 app.get("/:restaurant_id/menus", function(req, res) {
-    console.log('redirecting to menus');
+    //console.log('redirecting to menus');
     apiProxy.web(req, res, {target: menus});
 });
 
 app.all("/:restaurantID/reviews", function(req, res) {
-  console.log('redirecting to reviews');
+  //console.log('redirecting to reviews');
   apiProxy.web(req, res, {target: reviews});
 });
 
